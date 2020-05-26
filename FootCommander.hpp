3d65f669cab1d39ac219d55538765b173ac1d5d3
reutKeyshawn <reki8611@gmail.com>
2020-05-26 18:48:56 +0300

    update

--- FootCommander.hpp
+++ FootCommander.hpp
@@ -12,12 +12,10 @@ public:
     FootCommander(uint player_id, uint health_points = 150, uint damage = 20) : FootSoldier(player_id, health_points, damage) {}
     void attack(std::vector<std::vector<Soldier *>> &b, std::pair<int, int> location);
 
-    ~FootCommander();
+    // ~FootCommander();
 };
 
 
-FootCommander::~FootCommander()
-{
-}
+
 
 #endif
